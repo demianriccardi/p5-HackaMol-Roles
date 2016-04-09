cb46bf2eeb2d003ef36ffb78355219e137cc055d
demianriccardi <demianriccardi@gmail.com>
2016-04-09 15:58:05 -0400

    Simple selections have been implemented backbone, sidechains, water, protein, metals, ligands are all pretty trivial.  The next thing to work out is more complex selections like "(resname LYS .or. resname HIS) .and. chain A .and. occ >= 1".

--- SelectionRole/lib/HackaMol/Roles/SelectionRole.pm
+++ SelectionRole/lib/HackaMol/Roles/SelectionRole.pm
@@ -5,6 +5,15 @@ use Moose::Role;
 use HackaMol::AtomGroup;
 use Carp;
 
+my %common_selections = (
+    'backbone'    => sub {$_->record_name eq 'ATOM' and ($_->name eq 'N' or $_->name eq 'CA' or $_->name eq 'C')},
+    'water'       => sub {$_->resname =~ m/HOH|TIP|H2O/ and $_->record_name eq 'HETATM' }, 
+    'protein'     => sub {$_->record_name eq 'ATOM'},
+    'ligands'     => sub {($_->resname !~ m/HOH|TIP|H2O/) and $_->record_name eq 'HETATM' }, 
+    'metals'      => sub {my $atom = $_; grep {$atom->Z == $_} (3,4,11,12,19 .. 30, 37 .. 48, 55 .. 80) },
+    'sidechains'  => sub {$_->record_name eq 'ATOM' and not ($_->name eq 'N' or $_->name eq 'CA' or $_->name eq 'C')}
+);
+
 has 'selections' => (
     traits    => ['Hash'],
     is        => 'ro',
@@ -21,14 +30,21 @@ has 'selections' => (
 );
 
 sub select_group{
-  my $self = shift;
 
-  my $select_method = _method(@_);
+  my $self = shift;
+  my $selection = shift;
+  my $method;
+  if (exists($common_selections{$selection})) {
+    $method = $common_selections{$selection};
+  }
+  else {
+    $method = _method($selection);
+  }
   #grep { &{ sub{ $_%2 } }($_)} 1..10
 
   my $group = HackaMol::AtomGroup->new( 
                         atoms=>[ 
-                          grep {$select_method} $self->all_atoms
+                          grep{&{$method}($_)}  $self->all_atoms 
                         ],
   ); 
 
@@ -38,6 +54,11 @@ sub select_group{
 
 sub _method{
   my $str = shift;
+  print "$str not implemented yet"; return(sub{0});
+  my @ands = split ('.and.',$str);
+  my @ors  = map{[split ('.or.', $_)]}
+  my ($attr,$val) = split(' ', $str);
+
   return $str;
 }
 
--- SelectionRole/t/Roles/SelectionRole.t
+++ SelectionRole/t/Roles/SelectionRole.t
@@ -20,4 +20,14 @@ ensure_all_roles($mol, 'HackaMol::Roles::SelectionRole');
 map has_attribute_ok( $mol, $_ ), @attributes;
 map can_ok (          $mol, $_ ), @methods;
 
+my $backbone = $mol->select_group('backbone');
+my $water    = $mol->select_group('water');
+my $sidechains= $mol->select_group('sidechains');
+$sidechains->print_pdb; exit;
+#$backbone->print_pdb;
+
+my $metals  = $mol->select_group('metals');
+my $ligands = $mol->select_group('ligands');
+$ligands->print_pdb;
+
 done_testing();
