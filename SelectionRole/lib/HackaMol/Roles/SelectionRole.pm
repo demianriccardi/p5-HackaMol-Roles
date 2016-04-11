beeb27ee90e305bc83f9e2a115c49754b9942d5c
Demian Riccardi <demianriccardi@gmail.com>
2016-04-11 10:09:03 -0400

    major updates to API  Changes to be committed: 	modified:   Changes   modified:   dist.ini   modified:   lib/HackaMol/Roles/SelectionRole.pm   modified:   t/Roles/SelectionRole.t

--- SelectionRole/lib/HackaMol/Roles/SelectionRole.pm
+++ SelectionRole/lib/HackaMol/Roles/SelectionRole.pm
@@ -1,77 +1,82 @@
 package HackaMol::Roles::SelectionRole;
 
-#ABSTRACT: Atom selections in molecules 
+#ABSTRACT: Atom selections in molecules
 use Moose::Role;
 use HackaMol::AtomGroup;
 use Carp;
 
 my %common_selections = (
-    'backbone'    => sub {grep { $_->record_name eq 'ATOM' and ($_->name eq 'N' or $_->name eq 'CA' or $_->name eq 'C')} @_},
-    'water'       => sub {grep {$_->resname =~ m/HOH|TIP|H2O/ and $_->record_name eq 'HETATM' } @_ }, 
-    'protein'     => sub {grep {$_->record_name eq 'ATOM'} @_ },
-    'ligands'     => sub {grep {($_->resname !~ m/HOH|TIP|H2O/) and $_->record_name eq 'HETATM' } @_}, 
-    'metals'      => sub {grep {my $atom = $_; grep {$atom->Z == $_} (3,4,11,12,19 .. 30, 37 .. 48, 55 .. 80) } @_ },
-    'sidechains'  => sub {grep {$_->record_name eq 'ATOM' and 
-                                not ($_->name eq 'N' or $_->name eq 'CA' or $_->name eq 'C')} @_},
-    'test'        => sub {grep {($_->chain eq 'E' or ($_->resname eq 'TYR' and $_->chain eq 'I')) and $_->occ <= 1.0} @_},
+    'backbone'   => '$_->record_name eq "ATOM" and ( $_->name eq "N" or $_->name eq "CA" or $_->name eq "C" )',
+    'water'      => '$_->resname =~ m/HOH|TIP|H2O/ and $_->record_name eq "HETATM"',
+    'protein'    => '$_->record_name eq "ATOM"',
+    'ligands'    => '($_->resname !~ m/HOH|TIP|H2O/ ) and $_->record_name eq "HETATM"',
+    'sidechains' => '$_->record_name eq "ATOM"
+                     and not( $_->name eq "N" or $_->name eq "CA" or $_->name eq "C" )',
+    'metals'     => '$_->symbol =~ m/Li|Be|Na|Mg|K|Ca|Sc|Ti|V|Cr|Mn|Fe|Co|Ni|Cu|Zn|Rb|Sr|Y|Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|Cs|Ba|La|Ce|Pr|Nd|Pm|Sm|Eu|Gd|Tb|Dy|Ho|Er|Tm|Yb|Lu|Hf|Ta|W|Re|Os|Ir|Pt|Au|Hg/', 
 );
 
-has 'selections' => (
-    traits    => ['Hash'],
-    is        => 'ro',
-    isa       => 'HashRef[CodeRef]',
-    default   => sub { {} },
-    handles   => {
-        get_selection    => 'get',
-        set_selection    => 'set',
-        has_selections   => 'count',
-        keys_selection   => 'keys',
-        delete_selection => 'delete',
-        has_selection    => 'exists',
+has 'selections_cr' => (
+    traits  => ['Hash'],
+    is      => 'ro',
+    isa     => 'HashRef[CodeRef]',
+    default => sub { {} },
+    handles => {
+        get_selection_cr    => 'get',
+        set_selection_cr    => 'set',
+        has_selections_cr   => 'count',
+        keys_selection_cr   => 'keys',
+        delete_selection_cr => 'delete',
+        has_selection_cr    => 'exists',
     },
 );
 
-sub select_group{
+sub select_group {
 
-  my $self = shift;
-  my $selection = shift;
-  my $method;
-  if (exists($common_selections{$selection})) {
-    $method = $common_selections{$selection};
-  }
-  else {
-    $method = _regex_method($selection);
-  }
-  #grep { &{ sub{ $_%2 } }($_)} 1..10
+    my $self      = shift;
+    my $selection = shift;
+    my $method;
 
-  my $group = HackaMol::AtomGroup->new( 
-                        atoms=>[ 
-                          &{$method}($self->all_atoms) 
-                        ],
-  ); 
+    if ($self->has_selection_cr($selection)){ #attr takes priority so user can change
+        $method = $self->get_selection_cr($selection);
+    }
+    elsif ( exists( $common_selections{$selection} ) ) {
+        $method = eval("sub{ grep{ $common_selections{$selection} } \@_ }");
+    }
+    else {
+        $method = _regex_method($selection);
+    }
 
-  return($group);
+    #grep { &{ sub{ $_%2 } }($_)} 1..10
+
+    my $group =
+      HackaMol::AtomGroup->new( atoms => [ &{$method}( $self->all_atoms ) ], );
+
+    return ($group);
 
 }
 
 # $mol->select_group('(chain A .or. (resname TYR .and. chain B)) .and. occ .within. 1')
 # becomes grep{($_->chain eq A or ($_->resname eq TYR and $_->chain eq 'B')) and $_->occ <= 1.0}
 
-sub _regex_method{
-  my $str = shift;
-  #print "$str not implemented yet"; return(sub{0});
-  #my @parenth = $str =~ /(\(([^()]|(?R))*\))/g    
+sub _regex_method {
+    my $str = shift;
 
-  $str =~ s/(\w+)\s+([A-Za-z]+)/\$\_->$1 eq \'$2\'/g;
-  $str =~ s/(\w+)\s+(\d+)/\$\_->$1 == $2/g;
-  $str =~ s/(\w+)\s+\.within\.\s+(\d+)/\$\_->$1 <= $2/g;
-  $str =~ s/(\w+)\s+\.beyond\.\s+(\d+)/\$\_->$1 >= $2/g;
-  $str =~ s/\.and\./and/g;
-  $str =~ s/\.or\./or/g;
-  $str =~ s/\.not\./not/g;
+    #print "$str not implemented yet"; return(sub{0});
+    #my @parenth = $str =~ /(\(([^()]|(?R))*\))/g
 
-  return (eval( "sub{ grep{ $str } \@_ }" ) );
+    $str =~ s/(\w+)\s+([A-Za-z]+)/\$\_->$1 eq \'$2\'/g;
+    $str =~ s/(\w+)\s+(\d+)/\$\_->$1 == $2/g;
+    $str =~ s/(\w+)\s+\.within\.\s+(\d+)/\$\_->$1 <= $2/g;
+    $str =~ s/(\w+)\s+\.beyond\.\s+(\d+)/\$\_->$1 >= $2/g;
+    $str =~ s/$_/\($common_selections{$_}\)/g foreach keys %common_selections;
+    $str =~ s/\.and\./and/g;
+    $str =~ s/\.or\./or/g;
+    $str =~ s/\.not\./not/g;
+
+    return ( eval("sub{ grep{ $str } \@_ }") );
 }
+
+
 
 no Moose::Role;
 
@@ -151,9 +156,17 @@ For example,
                                                     } @_ }
       );
 
-=attr selections
+=attr selections_cr
 
-isa HashRef[CodeRef] that is lazy with public Hash traits.  
+isa HashRef[CodeRef] that is lazy with public Hash traits.  This attribute allows the user to use code references in the atom selections.
+The list of atoms, contained in the role consuming object, will be passed to the code reference, and a list of atoms is the expected output
+of the code reference, e.g.
+
+    @new_atoms = &{$code_ref}(@atoms);
+
+=method set_selections_cr
+
+two arguments: a string and a coderef
 
 =method select_group
 
