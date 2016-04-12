92edf8b269c9bc4ce0cfd04dea8fd777e5124b5e
demianriccardi <demianriccardi@gmail.com>
2016-04-11 20:34:24 -0400

    update path

--- examples/load_RoleFile_Selection.pl
+++ examples/load_RoleFile_Selection.pl
@@ -2,7 +2,7 @@ use HackaMol;
 use Module::Load;
 use Moose::Util qw( ensure_all_roles );
 
-load (shift); #path to SelectionRole role
+load ('SelectionRole/lib/HackaMol/Roles/SelectionRole.pm'); #path to SelectionRole role
 
 my $mol = HackaMol->new->pdbid_mol("2sic");
 ensure_all_roles($mol, 'HackaMol::Roles::SelectionRole');
